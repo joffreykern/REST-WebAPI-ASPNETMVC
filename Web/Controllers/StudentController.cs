8895df5a3ac90b7cc01bf8851ac3154fca4d4e7e
Joffrey Kern <joffrey.kern@live.fr>
2013-06-01 16:46:11 +0200

    CRUD done with a custom request

--- Web/Controllers/StudentController.cs
+++ Web/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -20,6 +21,47 @@ namespace Web.Controllers
         public List<Student> GetStudents()
         {
             return this.context.Students.ToList();
+        }
+
+        public Student GetStudent(int id)
+        {
+            return this.context.Students.Find(id);
+        }
+
+        [ActionName("name")]
+        public Student GetStudentByName(string param = "")
+        {
+            return new Student() {Firstname = param};
+        }
+            
+        [HttpPost]
+        public void Post(Student student)
+        {
+            this.context.Students.Add(student);
+            this.context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void Put(int id, Student student)
+        {
+            Student s = this.context.Students.Find(id);
+            if (s != null)
+            {
+                s.Firstname = student.Firstname;
+                s.Lastname = student.Lastname;
+                this.context.SaveChanges();
+            }
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            Student student = this.context.Students.Find(id);
+            if (student != null)
+            {
+                this.context.Students.Remove(student);
+                this.context.SaveChanges();
+            }
         }
 
         protected override void Dispose(bool disposing)
